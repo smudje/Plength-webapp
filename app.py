b70f9dc93435956f9553be3614a7819df36ffef1
nuwanwre <nuwanwre@live.com>
2019-04-26 15:42:58 +0900

    [NW] Added random filename generator for uploaded images

--- app.py
+++ app.py
@@ -8,13 +8,16 @@ from flask import (
 )
 from werkzeug.utils import secure_filename
 import base64
-import os 
+import os
+import random
+import string
 
 app = Flask(__name__)
 app.secret_key = 'Plength'
 app.debug = True
 UPLOAD_FOLDER = './uploads/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'bmp'])
+filename = ''
 
 @app.route('/')
 def hello_world():
@@ -25,10 +28,14 @@ def allowed_file(filename):
 
 @app.route('/uploadImage', methods=['GET', 'POST'])
 def upload_file():
-  # app.logger.debug(request.data)
-  f = open("imgData.txt", "wb")
-  f.write(request.data)
-  f.close()
-  #app.logger.debug(request.method)
-  with open("imageToSave.png", "wb") as fh:
-    fh.write(base64.decodebytes(request.data))
+  if request.data is None:
+    return ''
+  else:
+    # app.logger.debug(request.data)
+    # f = open("imgData.txt", "wb")
+    # f.write(request.data)
+    # f.close()
+    #app.logger.debug(request.method)
+    filename = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    with open(UPLOAD_FOLDER + filename + ".png", "wb") as fh:
+      fh.write(base64.decodebytes(request.data))
